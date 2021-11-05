5bca145e2b122027799e75e987c20c08db16c7b5
luclap <boylap7@gmail.com>
2021-11-05 19:40:19 +0700

    thay

--- Myproject/src/app/pp.java
+++ Myproject/src/app/pp.java
@@ -3,6 +3,6 @@ public class pp {
     public static void main(String[] args) throws Exception {
         Pet a = new Pet("kim",5, "đen");
         System.out.println(a);
-        System.out.println("strange");
+        System.out.println("ma");
     }
 }
